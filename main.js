1c2d4f024a952e35e43e6287f8ea04dc856f28d5
Temmie <77637324+temmiexva@users.noreply.github.com>
2023-03-05 15:04:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+let a
+function PowerA3(a){
+    let b = 1
+    for(let i = 0; i < 3; i++){
+        b *= a
+    }
+return b
+}
+*/
+/*
+function PowerA234(a){
+    let b = 1,c = 1, d =1
+    for(let i = 0; i < 2; i++){
+        b *= a
+    }
+    for(let m = 0; m < 3; m++){
+        c *= a
+    }
+    for(let n = 0; n < 4; n++){
+        d *= a
+    }
+    console.log(b)
+    console.log(c)
+    console.log(d)
+}
+*/
+/*
+let func1 = (function(){return 3})
+let func2 = (function(){return 4})
+
+function result(a,b){
+    return a * b
+}
+*/
+/*
+let kvadrat = (a) =>{ let result = 1
+  for(let i = 0; i < 2; i++){result *= a};
+  return result
+}
+
+let kub = (a) =>{ let result = 1
+    for(let i = 0; i < 3; i++){result *= a};
+    return result
+  }
+
+ let result = (x,y) => x + y;
+
+ */
+ 
+ 
